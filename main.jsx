45055964a5d362d5d3252b46f3ae3b7a9c23e8c7
lylakorea <contact.lyla.kr@gmail.com>
2025-06-24 10:23:00 +0900

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const App = () => <h1>Hello, Brewtrip</h1>
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '3rem', fontFamily: 'sans-serif' }}>
+      <h1>술캠 Brewtrip</h1>
+      <p>지역 캠핑장 기반의 양조장 & 전통주 정보 제공 플랫폼</p>
+    </div>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
