bcbe7ef7430a500a3dd5b424e69fa4d8abac8267
lylakorea <contact.lyla.kr@gmail.com>
2025-06-26 08:51:51 +0900

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,13 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './src/App';
 
-const App = () => {
-  return (
-    <div style={{ textAlign: 'center', paddingTop: '3rem', fontFamily: 'sans-serif' }}>
-      <h1>술캠 Brewtrip</h1>
-      <p>지역 캠핑장 기반의 양조장 & 전통주 정보 제공 플랫폼</p>
-    </div>
-  )
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+function App() {
+  return <div>BrewTrip App - 정식 구축 버전</div>;
+}
+export default App;
